b047387f6ff0ee45bafb14d90a5ba5e0db779ab3
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-09 17:00:25 +0900

    [Bronze IV] Title: Tabliczka, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/8674. Tabliczka/Tabliczka.cc
+++ 백준/Bronze/8674. Tabliczka/Tabliczka.cc
@@ -1,22 +1,19 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+import java.util.Arrays;
+import java.util.Scanner;
 
-int main(void)
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	long long a, b;
-	cin >> a >> b;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        long[] cho = new long[2];
+        cho[0] = scanner.nextLong();
+        cho[1] = scanner.nextLong();
 
-	if (a % 2 && b % 2)
-	{
-		cout << min(a, b) << "\n";
-	}
-	else
-	{
-		cout << 0 << "\n";
-	}
+        if(cho[0] % 2 == 0 || cho[1] % 2 == 0) System.out.println(0);
+        else {
+            Arrays.sort(cho);
+            System.out.println(cho[0]);
+        }
 
-	return 0;
+    }
 }
