35b304ea43cdfffe56d9db5f6b91ed28b550520d
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-10 02:24:13 +0000

    [Bronze IV] Title: 감정이입, Time: 208 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/14623. 감정이입/감정이입.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        BigInteger a = scanner.nextBigInteger(2);
+        BigInteger b = scanner.nextBigInteger(2);
+
+        BigInteger multiply = a.multiply(b);
+        String answer = multiply.toString(2);
+        
+        System.out.println(answer);
+
+
+    }
+}
