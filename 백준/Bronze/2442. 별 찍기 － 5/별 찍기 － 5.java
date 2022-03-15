bd52c48b88a35dee9fc22034a6a53406260edb73
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-15 20:04:36 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 372 ms, Memory: 18688 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cnt = scanner.nextInt();
+
+        for (int i = 0; i < cnt; i++) {
+            for (int k = cnt - 1; k > i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
