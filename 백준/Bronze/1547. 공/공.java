36c4dfd2d3f2ec92df7cb46ddfea023881d099b8
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-15 14:24:11 +0900

    [Bronze III] Title: 공, Time: 224 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// 공
+// https://www.acmicpc.net/problem/1547
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int num = scanner.nextInt();
+        int location = 1;
+        
+        for (int i = 0; i < num; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            
+            if (a == location) location = b;
+            else if (b == location) location = a;
+        }
+        
+        System.out.println(location);
+    }
+}
