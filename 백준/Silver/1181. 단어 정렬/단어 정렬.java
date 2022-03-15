19dd1b4c1ee571d5e26216c8f723dd31783ee71e
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-15 12:13:02 +0900

    [Silver V] Title: 단어 정렬, Time: 896 ms, Memory: 40268 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,40 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int cnt = scan.nextInt();
+        String[] sen = new String[cnt];
+        for (int i = 0; i < cnt; i++) {
+            sen[i] = scan.next();
+        }
+        lengthCompare(sen);
+
+        for (int i = 0; i < sen.length - 1; i++) {
+            if (sen[i].equals(sen[i + 1])) {
+                sen[i] = "";
+            }
+        }
+        for (String s : sen) {
+            if (s.length() != 0)
+                System.out.println(s);
+        }
+    }
+
+    public static String[] lengthCompare(String[] sen) {
+        Arrays.sort(sen, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                }
+                else
+                    return s1.length() - s2.length();
+            }
+        });
+        return sen;
+    }
+}
