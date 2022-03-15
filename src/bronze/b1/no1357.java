812d6e31ea45ce3a5407c920781fb8202ec19cce
m04j00 <m04j00@gmail.com>
2022-03-15 15:11:56 +0900

    bronze 문제 풀이

---
+++ src/bronze/b1/no1357.java
@@ -0,0 +1,30 @@
+package bronze.b1;
+
+import java.util.Scanner;
+
+public class no1357 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder x = new StringBuilder(scanner.next()).reverse();
+        StringBuilder y = new StringBuilder(scanner.next()).reverse();
+
+        int numX = Integer.parseInt(removeZero(x));
+        int numY = Integer.parseInt(removeZero(y));
+        String sum = Integer.toString(numX + numY);
+
+        StringBuilder answer = new StringBuilder(sum).reverse();
+
+        String ret = removeZero(answer);
+        System.out.println(ret);
+    }
+
+    public static String removeZero(StringBuilder str) {
+        if (str.length() == 1) return str.toString();
+
+        while (true) {
+            if (str.substring(0, 1).equals("0")) str = new StringBuilder(str.substring(1));
+            else break;
+        }
+        return str.toString();
+    }
+}
---
+++ src/bronze/b5/no18018.java
@@ -0,0 +1,11 @@
+package bronze.b5;
+
+import java.util.Scanner;
+
+public class no18018 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n - 543);
+    }
+}
