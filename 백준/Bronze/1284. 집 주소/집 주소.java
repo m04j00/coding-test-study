1894a3874fabedfdbc738455c83423ac2b863f8f
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-15 14:19:00 +0900

    [Bronze III] Title: 집 주소, Time: 192 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Bronze/1284. 집 주소/집 주소.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// 집 주소
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String num = scanner.next();
+            if (num.equals("0")) break;
+            
+            int answer = num.length() + 1;
+            
+            for (int i = 0; i < num.length(); i++) {
+                String n = num.substring(i, i + 1);
+                if (n.equals("1")) answer += 2;
+                else if (n.equals("0")) answer += 4;
+                else answer += 3;
+            }
+            
+            System.out.println(answer);
+        }
+    }
+}
