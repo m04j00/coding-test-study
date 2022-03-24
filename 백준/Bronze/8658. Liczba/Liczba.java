919df7b10d1c4cdbc1d0379dd78a4c3f4fcba262
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-24 10:20:54 +0900

    [Bronze III] Title: Liczba, Time: 240 ms, Memory: 18268 KB -BaekjoonHub

---
+++ 백준/Bronze/8658. Liczba/Liczba.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i != 0) {
+                System.out.print(i + " ");
+                break;
+            }
+        }
+        for (int i = n - 1; i >= 2; i--) {
+            if (n % i != 0) {
+                System.out.print(i + " ");
+                break;
+            }
+        }
+    }
+}
