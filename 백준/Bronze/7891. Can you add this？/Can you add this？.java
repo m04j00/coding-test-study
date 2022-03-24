0c37d21c2d19af51bde27a20667365f1945fd713
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-24 09:21:57 +0900

    [Bronze III] Title: Can you add this?, Time: 232 ms, Memory: 18048 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            BigInteger a = scanner.nextBigInteger();
+            BigInteger b = scanner.nextBigInteger();
+            System.out.println(a.add(b));
+        }
+
+    }
+}
