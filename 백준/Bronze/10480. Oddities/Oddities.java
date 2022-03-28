a1f3f3b589f3a9a3f17619f4486e874be3fff4fd
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-28 10:18:15 +0900

    [Bronze III] Title: Oddities, Time: 220 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Bronze/10480. Oddities/Oddities.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            if (n % 2 == 0) System.out.println(n + " is even");
+            else System.out.println(n + " is odd");
+        }
+    }
+}
