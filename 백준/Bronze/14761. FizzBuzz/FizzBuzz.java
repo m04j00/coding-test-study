65ce384291a3e9d3e20a02f7567aacfc089790f3
Min J <69101860+m04j00@users.noreply.github.com>
2022-03-31 11:06:37 +0900

    [Bronze III] Title: FizzBuzz, Time: 216 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/14761. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % x == 0 && i % y == 0) System.out.println("FizzBuzz");
+            else if (i % x == 0) System.out.println("Fizz");
+            else if (i % y == 0) System.out.println("Buzz");
+            else System.out.println(i);
+        }
+    }
+}
