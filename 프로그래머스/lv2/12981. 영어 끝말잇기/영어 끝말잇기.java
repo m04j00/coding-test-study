680ff809e36ea3720f958a7e116a66641508ed9d
Min J <69101860+m04j00@users.noreply.github.com>
2022-06-27 20:26:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,16 +2,16 @@ import java.util.HashSet;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        HashSet<String> map = new HashSet<>();
-        map.add(words[0]);
+        HashSet<String> set = new HashSet<>();
+        set.add(words[0]);
 
         for(int i = 1; i < words.length; i++){
             if(
                 words[i-1].substring(words[i-1].length() - 1).equals(
                     words[i].substring(0, 1))
-              && !map.contains(words[i])
+              && !set.contains(words[i])
             ) {
-                map.add(words[i]);
+                set.add(words[i]);
             }
             else{
                 answer[0] = i % n + 1;
