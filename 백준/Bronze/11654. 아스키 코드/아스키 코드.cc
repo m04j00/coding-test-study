7d5e98439110eb938d35bbb74ff624d94963be97
Min J <69101860+m04j00@users.noreply.github.com>
2024-02-04 23:18:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int output;
+	char input;
+	cin >> input;
+
+	//if(isupper(input) || islower(input)) 
+		output = (int)input;
+
+	cout << (int)output;
+}
