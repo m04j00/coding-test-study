d2b7fd8eda7a18d724aae6008093f451173dd1db
Min J <69101860+m04j00@users.noreply.github.com>
2025-10-19 17:29:03 +0900

    [Bronze III] Title: 웰컴 키트, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -2,16 +2,13 @@ import math
 
 def main():
     n = int(input())
-    shirt_size_list = list(map(int, input().split()))
+    shirt_sizes = list(map(int, input().split()))
     t, p = map(int, input().split())
 
-    shirt_bundle = 0
+    total_bundles = sum(math.ceil(size / t) for size in shirt_sizes)
 
-    for i in shirt_size_list:
-        shirt_bundle += math.ceil(i / t)
-
-    print(shirt_bundle)
-    print(math.floor(n / p), n % p)
+    print(total_bundles)
+    print(n // p, n % p)
 
 if __name__ == "__main__":
     main()
