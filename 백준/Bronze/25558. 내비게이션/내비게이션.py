c878e660a6c680a66e63ea25ddbd449cb4ebe76f
Min J <69101860+m04j00@users.noreply.github.com>
2025-10-23 23:38:50 +0900

    [Bronze II] Title: 내비게이션, Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25558. 내비게이션/내비게이션.py
+++ 백준/Bronze/25558. 내비게이션/내비게이션.py
@@ -1,28 +1,29 @@
-def calc(point1, point2) -> int:
-    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+import sys
+
+def calc(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
 def main():
-    results = []
+    input = sys.stdin.readline
     n = int(input())
-    point = list(map(int, input().split()))
-    start = (point[0], point[1])
-    end = (point[2], point[3])
+    sx, sy, ex, ey = map(int, input().split())
 
-    for i in range(n):
+    results = []
+    for i in range(1, n + 1):
         m = int(input())
-        before_point = start
-        result = 0
+        bx, by = sx, sy
+        total = 0
         for _ in range(m):
-            point = tuple(map(int, input().split()))
-            result += calc(before_point, point)
-            before_point = point
+            x, y = map(int, input().split())
+            total += calc(bx, by, x, y)
+            bx, by = x, y
 
-        results.append((i + 1, result + calc(before_point, end)))
+        total += calc(bx, by, ex, ey)
+        results.append((i, total))
 
     if results:
-        min_tuple = min(results, key=lambda x: x[1])
-        print(min_tuple[0])
+        print(min(results, key=lambda x: x[1])[0])
 
 
 if __name__ == "__main__":
