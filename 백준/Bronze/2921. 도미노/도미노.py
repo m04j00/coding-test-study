4c0bce1d0296e4d2badced0a22ec348a01513854
Min J <69101860+m04j00@users.noreply.github.com>
2025-10-23 22:59:01 +0900

    [Bronze III] Title: 도미노, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2921. 도미노/도미노.py
+++ 백준/Bronze/2921. 도미노/도미노.py
@@ -1,11 +1,8 @@
+# Pythonic ...
+
 def main():
     n = int(input())
-    total_point = 0
-    while n > 0:
-        for i in range(n, n + n + 1):
-            total_point += i
-        n -= 1
-
+    total_point = sum(sum(range(i, i + i + 1)) for i in range(n, 0, -1))
     print(total_point)
 
 
